35d2dbe1ce03b9ac5dd48dd76d0ee225a4c7cfba
Martin Kröner <kronerm9@gmail.com>
2023-02-27 19:44:47 +0100

    improvements

--- src/robot.rs
+++ src/robot.rs
@@ -53,7 +53,7 @@ impl RobotController {
         tracing::info!("disconnected!");
     }
 
-    async fn send(&mut self, msg: ServerMessage) -> Option<()> {
+    async fn send(&mut self, msg: &ServerMessage) -> Option<()> {
         if let Err(e) = self.socket.write_all(msg.to_string().as_bytes()).await {
             tracing::error!("connection interrupted! ({e:?})");
             return None;
@@ -109,7 +109,7 @@ impl RobotController {
             Ok(Ok(msg)) => match msg {
                 ClientMessage::FullPower => Some(()),
                 _ => {
-                    self.send(ServerMessage::LogicError).await;
+                    self.send(&ServerMessage::LogicError).await?;
                     None
                 }
             },
@@ -118,7 +118,7 @@ impl RobotController {
                 None
             }
             Ok(Err(_)) => {
-                self.send(ServerMessage::SyntaxError).await;
+                self.send(&ServerMessage::SyntaxError).await;
                 None
             }
         }
@@ -135,7 +135,7 @@ impl RobotController {
                     self.get::<MAX_LENGTH>().await
                 }
                 ClientMessage::FullPower => {
-                    self.send(ServerMessage::LogicError).await;
+                    self.send(&ServerMessage::LogicError).await?;
                     None
                 }
                 _ => Some(msg),
@@ -144,7 +144,7 @@ impl RobotController {
                 MessageReceivedError::TimedOut => return None,
                 _ => {
                     tracing::error!("{e}");
-                    self.send(ServerMessage::SyntaxError).await;
+                    self.send(&ServerMessage::SyntaxError).await?;
                     None
                 }
             },
@@ -155,24 +155,24 @@ impl RobotController {
         let msg = self.get::<20>().await?;
         let ClientMessage::String(name) = msg else {
             tracing::error!("wrong variant received: {msg:?}");
-            self.send(ServerMessage::SyntaxError).await;
+            self.send(&ServerMessage::SyntaxError).await?;
             return None;
         };
         tracing::debug!("name: {name:?}");
 
-        self.send(ServerMessage::KeyRequest).await;
+        self.send(&ServerMessage::KeyRequest).await;
 
         let msg = self.get::<12>().await?;
         let ClientMessage::Number(key_id) = msg else {
             tracing::error!("wrong variant received: {msg:?}");
-            self.send(ServerMessage::SyntaxError).await;
+            self.send(&ServerMessage::SyntaxError).await?;
             return None;
         };
         tracing::debug!("key_id: {key_id}");
 
         if key_id > 4 {
             tracing::info!("key_id: {key_id} is out of range, disconnecting...");
-            self.send(ServerMessage::KeyOutOfRangeError).await;
+            self.send(&ServerMessage::KeyOutOfRangeError).await?;
             return None;
         }
 
@@ -182,66 +182,61 @@ impl RobotController {
         let name_char_sum: u16 = name.into_bytes().into_iter().map(|x| x as u16).sum();
         let checksum = name_char_sum.wrapping_mul(1000);
         let server_checksum = checksum.wrapping_add(server_key);
-        self.send(ServerMessage::Confirmation(server_checksum))
-            .await;
+        self.send(&ServerMessage::Confirmation(server_checksum))
+            .await?;
 
         let msg = self.get::<12>().await?;
         let ClientMessage::Number(client_checksum) = msg else {
             tracing::error!("wrong variant received: {msg:?}");
-            self.send(ServerMessage::SyntaxError).await;
+            self.send(&ServerMessage::SyntaxError).await?;
             return None;
         };
         let Ok(client_checksum): Result<u16,_> = client_checksum.try_into() else {
-                tracing::error!("invalid client checksum!");
-                self.send(ServerMessage::SyntaxError).await;
-                return None;
+            tracing::error!("invalid client checksum!");
+            self.send(&ServerMessage::SyntaxError).await?;
+            return None;
         };
         if checksum != client_checksum.wrapping_sub(client_key) {
-            self.send(ServerMessage::LoginFailed).await;
+            self.send(&ServerMessage::LoginFailed).await?;
             return None;
         }
 
-        self.send(ServerMessage::OK).await;
+        self.send(&ServerMessage::OK).await?;
         Some(())
     }
 
     async fn pick_up_secret(&mut self) -> Option<()> {
-        self.send(ServerMessage::PickUp).await;
-        match self.get::<100>().await? {
-            ClientMessage::String(secret) => {
-                tracing::debug!("secret found: {secret:?}");
-                Some(())
-            }
-            ClientMessage::Number(secret) => {
-                tracing::debug!("secret found: {secret:?}");
-                Some(())
-            }
+        self.send(&ServerMessage::PickUp).await;
+        let secret = match self.get::<100>().await? {
+            ClientMessage::String(secret) => secret,
+            ClientMessage::Number(secret) => secret.to_string(),
             msg => {
                 tracing::error!("wrong variant received: {msg:?}");
-                self.send(ServerMessage::SyntaxError).await;
+                self.send(&ServerMessage::SyntaxError).await?;
                 return None;
             }
-        }
+        };
+        tracing::debug!("secret found: {secret:?}");
+        Some(())
     }
 
     async fn log_out(&mut self) {
         tracing::trace!("logging out...");
-        self.send(ServerMessage::Logout).await;
+        self.send(&ServerMessage::Logout).await;
         tracing::trace!("logged out!");
     }
 
     async fn move_forward(&mut self) -> Option<MoveResult> {
-        self.send(ServerMessage::Move).await;
+        self.send(&ServerMessage::Move).await;
         let msg = self.get::<12>().await?;
         let ClientMessage::Ok(new_x, new_y) = msg else {
             tracing::error!("wrong variant received: {msg:?}");
-            self.send(ServerMessage::SyntaxError).await;
+            self.send(&ServerMessage::SyntaxError).await;
             return None
         };
-        let mut rammed = false;
         if let Position::Known(x, y) = self.robot.position {
             if (new_x - x, new_y - y) == (0, 0) {
-                rammed = true;
+                return Some(MoveResult::Rammed);
             }
             if let Direction::Unknown = self.robot.direction {
                 self.robot.direction = match (new_x - x, new_y - y) {
@@ -255,49 +250,39 @@ impl RobotController {
             }
         }
         self.robot.position = Position::Known(new_x, new_y);
-        Some(if rammed {
-            MoveResult::Rammed
-        } else {
-            MoveResult::Ok
-        })
+        Some(MoveResult::Ok)
+    }
+
+    async fn turn(&mut self, turn_message: ServerMessage) -> Option<()> {
+        self.send(&turn_message).await;
+        let msg = self.get::<12>().await?;
+        let ClientMessage::Ok(x, y) = msg else {
+            tracing::error!("wrong variant received: {msg:?}");
+            self.send(&ServerMessage::SyntaxError).await;
+            return None
+        };
+        self.robot.position = Position::Known(x, y);
+        self.robot.direction = match (turn_message, &self.robot.direction) {
+            (_, Direction::Unknown) => Direction::Unknown,
+            (ServerMessage::TurnLeft, Direction::Up) => Direction::Left,
+            (ServerMessage::TurnLeft, Direction::Down) => Direction::Right,
+            (ServerMessage::TurnLeft, Direction::Left) => Direction::Down,
+            (ServerMessage::TurnLeft, Direction::Right) => Direction::Up,
+            (ServerMessage::TurnRight, Direction::Up) => Direction::Right,
+            (ServerMessage::TurnRight, Direction::Down) => Direction::Left,
+            (ServerMessage::TurnRight, Direction::Left) => Direction::Up,
+            (ServerMessage::TurnRight, Direction::Right) => Direction::Down,
+            _ => unreachable!(),
+        };
+        Some(())
     }
 
     async fn turn_left(&mut self) -> Option<()> {
-        self.send(ServerMessage::TurnLeft).await;
-        let msg = self.get::<12>().await?;
-        let ClientMessage::Ok(x, y) = msg else {
-            tracing::error!("wrong variant received: {msg:?}");
-            self.send(ServerMessage::SyntaxError).await;
-            return None
-        };
-        self.robot.position = Position::Known(x, y);
-        self.robot.direction = match self.robot.direction {
-            Direction::Unknown => Direction::Unknown,
-            Direction::Up => Direction::Left,
-            Direction::Down => Direction::Right,
-            Direction::Left => Direction::Down,
-            Direction::Right => Direction::Up,
-        };
-        Some(())
+        self.turn(ServerMessage::TurnLeft).await
     }
 
     async fn turn_right(&mut self) -> Option<()> {
-        self.send(ServerMessage::TurnRight).await;
-        let msg = self.get::<12>().await?;
-        let ClientMessage::Ok(x, y) = msg else {
-            tracing::error!("wrong variant received: {msg:?}");
-            self.send(ServerMessage::SyntaxError).await;
-            return None
-        };
-        self.robot.position = Position::Known(x, y);
-        self.robot.direction = match self.robot.direction {
-            Direction::Unknown => Direction::Unknown,
-            Direction::Up => Direction::Right,
-            Direction::Down => Direction::Left,
-            Direction::Left => Direction::Up,
-            Direction::Right => Direction::Down,
-        };
-        Some(())
+        self.turn(ServerMessage::TurnRight).await
     }
 
     async fn rotate(&mut self, direction: &Direction) -> Option<()> {
@@ -305,31 +290,29 @@ impl RobotController {
             (Direction::Up, Direction::Up)
             | (Direction::Down, Direction::Down)
             | (Direction::Left, Direction::Left)
-            | (Direction::Right, Direction::Right) => Some(()),
+            | (Direction::Right, Direction::Right) => (),
             (Direction::Up, Direction::Down)
             | (Direction::Down, Direction::Up)
             | (Direction::Left, Direction::Right)
             | (Direction::Right, Direction::Left) => {
                 self.turn_left().await?;
                 self.turn_left().await?;
-                Some(())
             }
             (Direction::Up, Direction::Left)
             | (Direction::Left, Direction::Down)
             | (Direction::Down, Direction::Right)
             | (Direction::Right, Direction::Up) => {
                 self.turn_left().await?;
-                Some(())
             }
             (Direction::Up, Direction::Right)
             | (Direction::Right, Direction::Down)
             | (Direction::Down, Direction::Left)
             | (Direction::Left, Direction::Up) => {
                 self.turn_right().await?;
-                Some(())
             }
             (Direction::Unknown, _) | (_, Direction::Unknown) => unreachable!(),
         }
+        Some(())
     }
 
     async fn acquire_initial_state(&mut self) -> Option<()> {
@@ -350,49 +333,46 @@ impl RobotController {
         loop {
             tracing::trace!("{:?}", self.robot);
             let Position::Known(x, y) = self.robot.position else { unreachable!() };
-            if x != 0 {
-                let direction = if x.is_negative() {
-                    Direction::Right
-                } else {
-                    Direction::Left
-                };
-                let avoid_direction = if y.is_negative() {
-                    Direction::Up
-                } else {
-                    Direction::Down
-                };
-                self.rotate(&direction).await?;
-                if let MoveResult::Rammed = self.move_forward().await? {
-                    tracing::trace!("avoiding obstacle!");
-                    self.rotate(&avoid_direction).await?;
-                    self.move_forward().await?;
-                    self.rotate(&direction).await?;
-                    self.move_forward().await?;
-                }
-                continue;
+            if (x, y) == (0, 0) {
+                break;
             }
-            if y != 0 {
-                let direction = if y.is_negative() {
-                    Direction::Up
-                } else {
-                    Direction::Down
-                };
-                let avoid_direction = if x.is_negative() {
-                    Direction::Right
-                } else {
-                    Direction::Left
-                };
+            let (direction, avoid_direction) = if x != 0 {
+                (
+                    if x.is_negative() {
+                        Direction::Right
+                    } else {
+                        Direction::Left
+                    },
+                    if y.is_negative() {
+                        Direction::Up
+                    } else {
+                        Direction::Down
+                    },
+                )
+            } else if y != 0 {
+                (
+                    if y.is_negative() {
+                        Direction::Up
+                    } else {
+                        Direction::Down
+                    },
+                    if x.is_negative() {
+                        Direction::Right
+                    } else {
+                        Direction::Left
+                    },
+                )
+            } else {
+                unreachable!()
+            };
+            self.rotate(&direction).await?;
+            if let MoveResult::Rammed = self.move_forward().await? {
+                tracing::trace!("avoiding obstacle!");
+                self.rotate(&avoid_direction).await?;
+                self.move_forward().await?;
                 self.rotate(&direction).await?;
-                if let MoveResult::Rammed = self.move_forward().await? {
-                    tracing::trace!("avoiding obstacle!");
-                    self.rotate(&avoid_direction).await?;
-                    self.move_forward().await?;
-                    self.rotate(&direction).await?;
-                    self.move_forward().await?;
-                }
-                continue;
+                self.move_forward().await?;
             }
-            break;
         }
 
         self.pick_up_secret().await?;
